926e93df7913c20f419daf43496d3cdf8ae17e3f
Raphael Wielandt <170776378+raphaelwielandt@users.noreply.github.com>
2025-06-24 18:02:43 +0200

    Create html/css calculator layout

--- main.js
+++ main.js
@@ -21,22 +21,19 @@ function divide(a, b) {
 function operate(operator, a, b) {
   switch (operator) {
     case '+':
-      add(a, b);
-      break;
+      return add(a, b);
 
     case '-':
-      subtract(a, b);
-      break;
+      return subtract(a, b);
 
     case '*':
-      multiply(a, b);
-      break;
+      return multiply(a, b);
 
     case '/':
-      divide(a, b);
-      break;
+      return divide(a, b);
 
     default:
       break;
   }
 }
+
